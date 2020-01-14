d2b4058b7505e6e4c4e8a09ed817457589af411f
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-14 21:08:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end 
   
   def execute_transaction 
-    if sender.valid? && sender.balance >= amount 
+    if self.valid? && sender.balance >= amount 
       sender.balance -= amount 
       receiver.balance += amount 
       self.status = "complete"
